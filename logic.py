54afcb8f3e025e372279b214ec0871e5ef997a48
dasha <dasha.kurushina@gmail.com>
2018-10-09 20:56:03 +0500

    начали создавать логику программы

---
+++ logic.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  9 20:30:20 2018
+
+@author: dasha
+"""
+
+def parseInputString(inputString = "avb&(c⊕d)"):
+    rez = {
+            "Var":['a','b','c','d'],
+            "Ops":[]}
+    return rez
+
+if __name__ == "__main__":
+    print (parseInputString())
